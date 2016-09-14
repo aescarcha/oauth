55165e8b5f722c7505d9460005b2042b3d986360
aEscarcha <alvaro.escarcha@gmail.com>
2016-09-14 18:38:19 +0200

    Full functional test where a client is created and a token for a user is generated

--- Tests/Functional/FunctionalTest.php
+++ Tests/Functional/FunctionalTest.php
@@ -11,7 +11,9 @@ class FunctionalTest extends WebTestCase
 
     public function setUp()
     {
-        $this->loadFixtures(array());
+        $this->loadFixtures(array(
+            'Aescarcha\UserBundle\DataFixtures\ORM\LoadUserData',
+                                  ));
         $this->client = static::createClient();
         $this->manager = $this->client->getContainer()->get('doctrine.orm.entity_manager');
     }
@@ -21,21 +23,28 @@ class FunctionalTest extends WebTestCase
         $clientManager = $this->client->getContainer()->get('fos_oauth_server.client_manager.default');
         $client = $clientManager->createClient();
         $client->setRedirectUris(array('http://www.example.com'));
-        $client->setAllowedGrantTypes(array('token', 'authorization_code'));
+        $client->setAllowedGrantTypes(array('token', 'authorization_code', 'password'));
         $clientManager->updateClient($client);
-
-        // $crawler = $this->client->request(
-        //                  'POST',
-        //                  '/businesses',
-        //                  array(),
-        //                  array(),
-        //                  array('CONTENT_TYPE' => 'application/json'),
-        //                  '{"name":"my unit test"}'
-        //                  );
-        // $this->assertEquals(201, $this->client->getResponse()->getStatusCode());
-        // $response = json_decode($this->client->getResponse()->getContent(), true);
-        // $this->assertEquals( 'my unit test', $response['data']['name'] );
-        // $this->assertContains( '/businesses/', $response['data']['links']['self']['uri'] );
+        $crawler = $this->client->request(
+                         'POST',
+                         '/oauth/v2/token',
+                         array(),
+                         array(),
+                         array('CONTENT_TYPE' => 'application/json'),
+                         '{
+                           "grant_type":"password",
+                           "client_id":"' . $client->getId() . '_' . $client->getRandomId() . '",
+                           "client_secret":"' . $client->getSecret() . '",
+                           "username": "Alvaro",
+                           "password": "1231251265"
+                          }'
+                         );
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+        $response = json_decode($this->client->getResponse()->getContent(), true);
+        $this->assertTrue( strlen($response['access_token']) > 10 );
+        $this->assertEquals( 'bearer', $response['token_type'] );
+        $this->assertTrue( strlen($response['refresh_token']) > 10 );
+        $this->assertEquals( 3600, $response['expires_in'] );
     }
 
 
